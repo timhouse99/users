e5f0487e50cb38619f3649201f7060bc1a2c8959
timhouse99 <houset1@nku.edu>
2018-12-04 23:07:49 +0000

    Update person.rb

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ApplicationRecord
+  attr_accessible :first_name, :last_name, :middle_initial, :suffix, :department_id, :role, :phone_number, :emails_address
+
+  validates :first_name, :last_name
+            :presence => true
 end
