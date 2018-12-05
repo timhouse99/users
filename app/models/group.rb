4bad99e36298d4729a8298c1092a87058b6c198c
timhouse99 <houset1@nku.edu>
2018-12-05 18:26:09 +0000

    Update models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,3 @@
 class Group < ApplicationRecord
-  has_and_belongs_to_many :UserAccount
+  #has_and_belongs_to_many :UserAccount
 end
--- app/models/user_accounts_group.rb
+++ app/models/user_accounts_group.rb
@@ -1,6 +1,6 @@
 class UserAccountsGroup < ApplicationRecord
-	belongs_to :UserAccount
+	#belongs_to :UserAccount
 	validates :account_id, presence: true
-	belongs_to :Group
+	#belongs_to :Group
 	validates :group_id, presence: true
 end
