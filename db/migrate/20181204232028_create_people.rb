7c4582e4c92f3ae6729a6e6ffb375f949186bd8f
timhouse99 <houset1@nku.edu>
2018-12-05 15:46:54 +0000

    Update people record

--- db/migrate/20181204232028_create_people.rb
+++ db/migrate/20181204232028_create_people.rb
@@ -1,6 +1,6 @@
 class CreatePeople < ActiveRecord::Migration[5.1]
   def change
-    change_table :people do |t|
+    create_table :people do |t|
       t.string :first_name
       t.string :last_name
       t.string :middle_initial
